4675559c02da5973db8d9f05b3866bebe979b4bc
Alain Muls <alain.muls@gmail.com>
2024-07-04 16:56:44 +0200

    creating constants for RTK pos file

---
+++ rtkpos/rtk_constants.py
@@ -0,0 +1,9 @@
+# dict containing the Quality modes
+dict_rtk_pvtmode = {
+    1: dict(desc="RTK with ﬁxed ambiguities", color="green"),
+    2: dict(desc="RTK with ﬂoat ambiguities", color="orange"),
+    3: dict(desc="SBAS aided PVT", color="deepskyblue"),
+    4: dict(desc="Differential PVT", color="darkcyan"),
+    5: dict(desc="Stand-Alone PVT", color="cornflowerblue"),
+    6: dict(desc="Precise Point Positioning (PPP)", color="limegreen"),
+}
