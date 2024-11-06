c436a829bc22cfb8bb7806a181ddc760c843df7d
Alain Muls <alain.muls@gmail.com>
2024-11-06 13:02:59 +0100

    first part in conversion of RINEX Obs to CSV per GNSS

--- rnxobs_csv.py
+++ rnxobs_csv.py
@@ -4,7 +4,12 @@ import os
 import sys
 from logging import Logger
 
+import polars as pl
+
+from config import ERROR_CODES
+from rinex.rinex_class import RINEX
 from utils import argument_parser, init_logger
+from utils.utilities import str_green
 
 
 def rnxobs_csv(argv: list):
@@ -18,7 +23,29 @@ def rnxobs_csv(argv: list):
 
     # parse the CLI arguments
     args_parsed = argument_parser.argument_parser_rnxobs_csv(args=argv[1:])
-    print(f"\nParsed arguments: {args_parsed}")
+
+    # create the file/console logger
+    logger = init_logger.logger_setup(args=args_parsed, base_name=script_name)
+    logger.info(f"Parsed arguments: {args_parsed}")
+
+    # create the RINEX object
+    try:
+        rnxobs = RINEX(
+            rnx_fn=args_parsed.rnx_fn,
+            gnss=args_parsed.gnss,
+            logger=logger,
+        )
+    except Exception as e:
+        logger.error(f"Error creating SBF object: {e}")
+        sys.exit(ERROR_CODES["E_NO_RINEX_OBS"])
+
+    # convert RINEX observation file to CSV file using gfzrnx
+    tabobs_dfs = rnxobs.gfzrnx_tabobs()
+    with pl.Config(tbl_cols=-1):
+        for gnss, tabobs_df in tabobs_dfs.items():
+            logger.debug(
+                f"Converted RINEX observation file for {gnss} to tabular observation file: \n{tabobs_df}"
+            )
 
     # create the file/console logger
     logger = init_logger.logger_setup(args=args_parsed, base_name=script_name)
