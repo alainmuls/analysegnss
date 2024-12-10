c82193c8f798ee0f36883fa38dfbb5d95ba92626
Alain Muls <alain.muls@gmail.com>
2024-12-10 12:15:09 +0100

    added rinex to csv conversion

--- setup.py
+++ setup.py
@@ -10,6 +10,9 @@ setup(
             "ppk_rnx2rtkp=analysegnss.rtkpos.ppk_rnx2rtkp:main",
             "ebh_lines=analysegnss.scripts.ebh.ebh_lines:main",
             "rtkppk_plot=analysegnss.plots.rtkppk_plot:main",
+            "rnx_csv=analysegnss.rinex.rnx_csv:main",
+            "rnxobs_csv=analysegnss.rinex.rnxobs_csv:main",
+            "rnxnav_csv=analysegnss.rinex.rnxnav_csv:main",
         ]
     },
 )
