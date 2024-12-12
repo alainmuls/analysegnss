8a862d30120e65d41854f300789be1bc795511bf
Alain Muls <alain.muls@gmail.com>
2024-12-12 15:44:16 +0100

    adding some constants

---
+++ src/analysegnss/glabng/glab_constants.py
@@ -0,0 +1,15 @@
+PROCESSING_MODE = {
+    0: "SPP",
+    1: "PPP",
+    2: "Reserved",
+    3: "Reserved",
+    4: "Reserved",
+    5: "SBAS 1 Frequency",
+    6: "SBAS DFMC",
+    7: "DGNSS",
+}
+
+PROCESSING_DIRECTION = {
+    0: "Forward processing",
+    1: "Backwards processing (not possible in SBAS and DGNSS modes)",
+}
