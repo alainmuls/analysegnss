4bbab18b56bb605d3bfa685719dec34fb866bd36
Alain Muls <alain.muls@gmail.com>
2025-03-11 16:16:14 +0100

    moved from setup.py to toml description of project

--- setup.py
+++ setup.py
@@ -1,22 +1,5 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
-    name="analysegnss",
-    packages=find_packages(where="src"),
     package_dir={"": "src"},
-    entry_points={
-        "console_scripts": [
-            "rtk_pvtgeod=analysegnss.sbf.rtk_pvtgeod:main",
-            "ppk_rnx2rtkp=analysegnss.rtkpos.ppk_rnx2rtkp:main",
-            "plot_coords=analysegnss.plots.plot_coords:main",
-            "rnx_csv=analysegnss.rinex.rnx_csv:main",
-            "rnxobs_csv=analysegnss.rinex.rnxobs_csv:main",
-            "rnxnav_csv=analysegnss.rinex.rnxnav_csv:main",
-            "ebh_lines=analysegnss.scripts.ebh.ebh_lines:main",
-            "glab_parser=analysegnss.glabng.glab_parser:main",
-            "cn0_dataframe=analysegnss.analyse.cn0_dataframe:main",
-            "sbfmeas_csv=analysegnss.sbf.sbfmeas_csv:main",
-            "sbfnav_csv=analysegnss.sbf.sbfnav_csv:main",
-        ]
-    },
 )
