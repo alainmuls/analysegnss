107feb15af41c38f43f4cb309f5816943fa95a0e
Alain Muls <alain.muls@gmail.com>
2025-03-19 11:54:37 +0100

    corrected error where some fields in CSV file started with a "space" which didn't allow the conversion to float value

--- src/analysegnss/rinex/rinex_nav_class.py
+++ src/analysegnss/rinex/rinex_nav_class.py
@@ -2,12 +2,19 @@ import os
 import subprocess
 from dataclasses import dataclass, field
 from io import StringIO
-import polars as pl
 
-from analysegnss.rinex.rinex_class import RINEX
-from analysegnss.config import DICT_GNSS, rich_console
+import polars as pl
+from rich import print as rprint
+
 import analysegnss.rinex.rinex_column_names as rcn
-from analysegnss.utils.utilities import str_green, str_red, str_yellow
+from analysegnss.config import DICT_GNSS, rich_console
+from analysegnss.rinex.rinex_class import RINEX
+from analysegnss.utils.utilities import (
+    str_green,
+    str_red,
+    str_yellow,
+    print_df_in_chunks,
+)
 
 
 @dataclass
@@ -72,10 +79,8 @@ class RINEX_NAV(RINEX):
                     if self.logger is not None:
                         self.logger.error(
                             f"File is not a RINEX navigation file: {self.rnxnav_ifn}"
-                            f"File is not a RINEX navigation file: {self.rnxnav_ifn}"
                         )
                     raise ValueError(
-                        f"File is not a RINEX navigation file: {self.rnxnav_ifn}"
                         f"File is not a RINEX navigation file: {self.rnxnav_ifn}"
                     )
         except Exception as e:
@@ -99,7 +104,6 @@ class RINEX_NAV(RINEX):
             "-f",  # overwrite previous version of the output file
             "-finp",
             self.rnxnav_ifn,
-            self.rnxnav_ifn,
             "-tab",
             "-tab_sep",
             ",",
@@ -116,7 +120,7 @@ class RINEX_NAV(RINEX):
             gfzrnx_args.extend(["-te", self.end_time.strftime("%H:%M:%S")])
 
         if self.logger is not None:
-            self.logger.debug(f"running: {' '.join(gfzrnx_args)}")
+            self.logger.debug(f"running: {str_yellow(' '.join(gfzrnx_args))}")
 
         # we have to process GLONASS apart from any other GNSS
         gnss_nav_dict = {}
@@ -128,10 +132,12 @@ class RINEX_NAV(RINEX):
         # Add other GNSS systems first if present
         if other_gnss:
             if self.logger is not None:
-                self.logger.debug(f"Processing GNSS: {other_gnss}")
+                self.logger.debug(
+                    f"Processing GNSS: {str_green(', '.join(other_gnss))}\n"
+                )
             # add a spinner while waiting for the conversion to complete
             with rich_console.status(
-                "Please wait - Processing GNSS:...", spinner="aesthetic"
+                "Please wait - Processing GNSS:...\n", spinner="aesthetic"
             ):
                 # process GEC systems without Glonass
                 gfzrnx_args.extend(["-satsys", "".join(other_gnss)])
@@ -140,7 +146,7 @@ class RINEX_NAV(RINEX):
                 gec_nav_dict = self.gnss_nav_to_tabnav(gfzrnx_opts=gfzrnx_args)
                 gnss_nav_dict.update(gec_nav_dict)
 
-            rich_console.print(f"GNSS {other_gnss} processed.")
+            rich_console.print(f"GNSS [green]{other_gnss}[/green] processed.")
 
         # Add GLONASS separately if present
         if has_glonass:
@@ -152,10 +158,10 @@ class RINEX_NAV(RINEX):
 
             # print(f"GLONASS gfzrnx_args = {' '.join(gfzrnx_args)}")
             if self.logger is not None:
-                self.logger.debug("Processing GNSS: R")
+                self.logger.debug(f"str_yellow('Processing GNSS: R')")
             # add a spinner while waiting for the conversion to complete
             with rich_console.status(
-                "Please wait - Processing GNSS R:...", spinner="aesthetic"
+                "Please wait - Processing GNSS R:...\n", spinner="aesthetic"
             ):
                 gfzrnx_args.extend(["-satsys", "R"])
                 # print(f"GLONASS gfzrnx_args = {' '.join(gfzrnx_args)}")
@@ -163,19 +169,14 @@ class RINEX_NAV(RINEX):
                 r_nav_dict = self.gnss_nav_to_tabnav(gfzrnx_opts=gfzrnx_args)
                 if r_nav_dict is not None:
                     gnss_nav_dict.update(r_nav_dict)
-                    
-            rich_console.print(f"GNSS {has_glonass} processed.")
-            
-            r_nav_dict = self.gnss_nav_to_tabnav(gfzrnx_opts=gfzrnx_args)
-            if r_nav_dict is not None:
-                gnss_nav_dict.update(r_nav_dict)
-                    
-            rich_console.print(f"GNSS {has_glonass} processed.")
+
+            rich_console.print(f"GNSS [green]{has_glonass}[/green] processed.")
 
         return gnss_nav_dict
 
     def gnss_nav_to_tabnav(self, gfzrnx_opts: list) -> dict:
-        print(f"gfzrnx_opts = {gfzrnx_opts}")
+        # rprint(f"gfzrnx_opts = {gfzrnx_opts}")
+
         try:
             result = subprocess.run(
                 gfzrnx_opts, capture_output=True, text=True, check=True
@@ -193,33 +194,18 @@ class RINEX_NAV(RINEX):
                 self.logger.error(
                     f"{str_red('gfzrnx conversion failed, check availability')} of {str_yellow(gnss_list)} "
                     f"in {str_yellow(self.rnxnav_ifn)}"
-                    f"in {str_yellow(self.rnxnav_ifn)}"
                 )
-                
+
                 result = None
-            # raise RuntimeError(
-            #     f"{str_red('gfzrnx conversion failed, check availability')} of {str_yellow(gnss_list)} "
-            #     f"in {str_yellow(self.rnxnav_ifn)}"
-            # )
-                
-                result = None
-            # raise RuntimeError(
-            #     f"{str_red('gfzrnx conversion failed, check availability')} of {str_yellow(gnss_list)} "
-            #     f"in {str_yellow(self.rnxnav_ifn)}"
-            # )
-        except PermissionError as e:
-            if self.logger:
-                self.logger.error(
-                    f"{str_red('Permission error running gfzrnx')}: {str(e)}"
-                )
-            raise PermissionError(
-                f"{str_red('Permission error running gfzrnx')}: {str(e)}"
-            )
+                # raise RuntimeError(
+                #     f"{str_red('gfzrnx conversion failed, check availability')} of {str_yellow(gnss_list)} "
+                #     f"in {str_yellow(self.rnxnav_ifn)}"
+                # )
 
         # check if selected GNSS has data in result.stdout
         if result is None:
             return
-        
+
         # read the first line of the output to get initial part of column names
         hdr_line = result.stdout.split("\n")[0]
 
@@ -227,11 +213,9 @@ class RINEX_NAV(RINEX):
         df_all_nav = pl.read_csv(
             StringIO(result.stdout), has_header=False, separator=",", skip_rows=1
         )
-        #     self.logger.debug(
-        #         f"Converted RINEX navigation file to tabular navigation file for "
-        #         f"{str_green(', '.join([GNSS_DICT[gnss] for gnss in self.gnss]))}: \n"
-        #         f"{df_all_nav}"
-        #     )
+
+        # sent to logger
+        self.logger.debug("Converted RINEX navigation file to tabular navigation file")
 
         # Group by GNSS type (column_2) and navigation type (column_7) and create
         # dictionary of DataFrames, key of dataframe is the tuple (gnss_type, nav_type)
@@ -244,12 +228,13 @@ class RINEX_NAV(RINEX):
 
         # obtained tabular navigation files for selected GNSS systems, process each GNSS sub-DataFrame
         for (gnss, nav_type), tabnav_df in nav_dict.items():
-            # if self.logger is not None:
-            #         self.logger.debug(
-            #             f"Correcting headers of navigation dataframe for "
-            #             f"{str_green(GNSS_DICT[gnss])} - {str_green(nav_type)}: \n"
-            #             f"{tabnav_df}"
-            #         )
+            gnss_name = DICT_GNSS[gnss]["abbrev"]
+            if self.logger is not None:
+                self.logger.debug(
+                    f"Setting column names for navigation dataframe: "
+                    f"{str_green(gnss_name)} - {str_green(nav_type)}: \n"
+                    # f"{tabnav_df}"
+                )
             # set correct columns names to each DataFrame
             colnames_hdr = hdr_line.split(",DATA")[0].split(",")
             colnames_nav = rcn.get_nav_param_names(gnss, nav_type).split(",")
@@ -257,24 +242,82 @@ class RINEX_NAV(RINEX):
 
             # rename the columns of the DataFrame
             tabnav_df.columns = new_columns
+            # print(
+            #     f"{print_df_in_chunks(title=f"tabnav_df[{gnss_name}, {nav_type}]", df=tabnav_df)}"
+            # )
+            # rprint(f"tabnav_df.columns = {tabnav_df.columns}")
 
-            # Convert columns with null-safe approach
+            # Correct way to cast columns
             columns_to_convert = {
-                "TIME": (pl.col("TIME").cast(pl.Int64, strict=False)),
-                "tk": (pl.col("tk").cast(pl.Int64, strict=False)),
-                "health": (pl.col("health").cast(pl.Int64, strict=False)),
-                "freqNum": (pl.col("freqNum").cast(pl.Int16, strict=False)),
-                "age": (pl.col("age").cast(pl.Int32, strict=False)),
-                "TauN": (pl.col("TauN").str.strip().cast(pl.Float64, strict=False)),
+                "TIME": pl.Int64,
+                "tk": pl.Int64,
+                "health": pl.Int64,
+                "freqNum": pl.Int16,
+                "age": pl.Int32,
+                "TauN": pl.Float64,
+                "af0": pl.Float64,
+                "af1": pl.Float64,
+                "af2": pl.Float64,
             }
 
-            for col, col_format in columns_to_convert.items():
+            for col, dtype in columns_to_convert.items():
                 if col in tabnav_df.columns:
-                    tabnav_df = tabnav_df.with_columns(col_format)
+                    if dtype == pl.Float64:
+                        # Check if the column is already a float
+                        if tabnav_df[col].dtype != pl.Float64:
+                            # Check for non-numeric values before conversion
+                            # non_numeric_values = tabnav_df.filter(
+                            #     ~pl.col(col).str.contains(r"^\s*[+-]?\d*\.?\d+$")
+                            # )[col].unique()
+                            # if non_numeric_values.len() > 0:
+                            #     self.logger.warning(
+                            #         f"Non-numeric values found in column '{col}': {non_numeric_values}"
+                            #     )
+                            # print(f"before regex:\n{tabnav_df.select(col).head(5)}")
+                            tabnav_df = tabnav_df.with_columns(
+                                pl.col(col)
+                                .str.replace_all(r"[^0-9\.\-\+e]", "")
+                                .alias(col)
+                            )
+                            # print(
+                            #     f"After removing non-numeric characters:\n{tabnav_df.select(col).head(5)}"
+                            # )
+                            tabnav_df = tabnav_df.with_columns(
+                                pl.col(col).str.replace_all(r"^\.", "0.").alias(col)
+                            )
+                            # print(
+                            #     f"After adding 0. to leading '.':\n{tabnav_df.select(col).head(5)}"
+                            # )
+
+                            # replace "e" by "E"
+                            tabnav_df = tabnav_df.with_columns(
+                                pl.col(col)
+                                .str.replace_all(r"e([+-]\d+)", "E$1")
+                                .alias(col)
+                            )
+                            # print(
+                            #     f"After replacing 'e' with 'E':\n{tabnav_df.select(col).head(5)}"
+                            # )
+                            # Now cast to float
+                            tabnav_df = tabnav_df.with_columns(
+                                pl.col(col).cast(pl.Float64, strict=False)
+                            )
+                            # with pl.Config() as cfg:
+                            #     cfg.set_float_precision(None)
+                            #     rprint(
+                            #         f"After casting to {dtype}:\n{tabnav_df.select(col).head(5)}"
+                            #     )
+                    else:
+                        tabnav_df = tabnav_df.with_columns(
+                            pl.col(col).cast(dtype, strict=False)
+                        )
 
             # remove duplicate rows from tabnav_df
             tabnav_df = tabnav_df.unique()
-            self.logger.debug(f"tabnav_df = \n{tabnav_df}")
+            # self.logger.debug(
+            #     f"tabnav_df[{str_green(DICT_GNSS[gnss]['abbrev'])}, {str_green(nav_type)}] = \n"
+            #     f"{print_df_in_chunks(title=f'tabnav_df[{gnss_name}, {nav_type}]', df=tabnav_df)}"
+            # )
 
             # adjust the DATE/TIME to get WKNR and TOW columns
             tabnav_df = (
@@ -298,11 +341,14 @@ class RINEX_NAV(RINEX):
             # sent to logger
             if self.logger is not None:
                 self.logger.debug(
-                    f"tabnav_df[{str_green(DICT_GNSS[gnss]["name"])}, {str_green(nav_type)}] = \n{tabnav_df}"
+                    f"tabnav_df[{str_green(DICT_GNSS[gnss]["name"])}, {str_green(nav_type)}] = \n"
+                    f"{print_df_in_chunks(title=f'tabnav_df[{gnss_name}, {nav_type}]', df=tabnav_df)}"
                 )
 
             # replace the tabnav_df on nav_dict after having renamed the columns
             nav_dict[(gnss, nav_type)] = tabnav_df
+
+            # input(str_yellow("press key to continue..."))
 
         # return the dictionary of DataFrames with keys of tuples (gnss, nav_type)
         return nav_dict
