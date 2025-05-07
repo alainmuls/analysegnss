5c44c8a9a34b26962fe02c860a205e268e87d33b
Alain Muls <alain.muls@gmail.com>
2025-05-07 16:50:05 +0200

    start working on ublox decoding

---
+++ src/analysegnss/ublox/ubx_file.py
@@ -0,0 +1,8 @@
+from pyubx2 import UBXReader, UBX_PROTOCOL
+
+with open(
+    "./data/ublox/2025-4-26_132532_serial-COM3_prise_statique_V1.ubx", "rb"
+) as stream:
+    ubr = UBXReader(stream, protfilter=UBX_PROTOCOL)
+    for raw_data, parsed_data in ubr:
+        print(parsed_data)
